494648db4a7198275bc4f79f282d2a91bf366729
thue101 <56626050+thue101@users.noreply.github.com>
2020-07-20 18:40:39 +0200

    Configured swagger by adding dependency and configuration class

--- src/main/java/com/thulani/conferenceapp/controllers/SpeakersController.java
+++ src/main/java/com/thulani/conferenceapp/controllers/SpeakersController.java
@@ -2,7 +2,9 @@ package com.thulani.conferenceapp.controllers;
 
 
 import com.thulani.conferenceapp.models.Session;
+import com.thulani.conferenceapp.models.Speaker;
 import com.thulani.conferenceapp.repositories.SessionRepository;
+import com.thulani.conferenceapp.repositories.SpeakerRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,39 +12,42 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping ("/api/v1/sessions")
+@RequestMapping("/api/v1/speakers")
 public class SpeakersController {
-    @Autowired
-    private SessionRepository sessionRepository;
 
-    @GetMapping
-    public List<Session> list() {
-        return sessionRepository.findAll();
-    }
+        @Autowired
+        private SpeakerRepository speakerRepository;
 
-    @GetMapping
-    @RequestMapping("{id}")
-    public Session get(@PathVariable Long id) {
-        return sessionRepository.getOne(id);
-    }
+        @GetMapping
+        public List<Speaker> list() {
+            return speakerRepository.findAll();
+        }
 
-    @PostMapping
-    public Session create(@RequestBody final Session session){
-        return sessionRepository.saveAndFlush(session);
-    }
+        @GetMapping
+        @RequestMapping("{id}")
+        public Speaker get(@PathVariable Long id) {
+            return speakerRepository.getOne(id);
+        }
 
-    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable Long id) {
-        //Also need to check for children records before deleting.
-        sessionRepository.deleteById(id);
-    }
+        @PostMapping
+        public Speaker create(@RequestBody Speaker speaker){
+            return speakerRepository.saveAndFlush(speaker);
+        }
 
-    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
-    public Session update(@PathVariable Long id, @RequestBody Session session) {
-        //because this is a PUT, we expect all attributes to be passed in. A PATCH would only need what has changed.
-        //TODO: Add validation that all attributes are passed in, otherwise return a 400 bad payload
-        Session existingSession = sessionRepository.getOne(id);
-        BeanUtils.copyProperties(session, existingSession, "session_id");
-        return sessionRepository.saveAndFlush(existingSession);
-    }
+        @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+        public void delete(@PathVariable Long id) {
+            //Also need to check for children records before deleting.
+            speakerRepository.deleteById(id);
+        }
+
+        @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+        public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker) {
+            //because this is a PUT, we expect all attributes to be passed in. A PATCH would only need what has changed.
+            //TODO: Add validation that all attributes are passed in, otherwise return a 400 bad payload
+            Speaker existingSpeaker = speakerRepository.getOne(id);
+            BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+            return speakerRepository.saveAndFlush(existingSpeaker);
+        }
+
+
 }
